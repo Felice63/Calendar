e92d8d0261dd9035ce68cb6b49fee552b0e93f96
Felice63 <fortefelice63@gmail.com>
2024-06-01 14:59:21 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@
 // Get the current date and time
 let now = new Date();
 
-// Get the HTML element with ID of month
+// Get the HTML element with id="month" and assign it to the monthAndYear variable
 let monthAndYear = document.querySelector(`#month`);
 
-// Set the innerHTML of the monthAndYear element to the current date
-// This apears at the top of the calendar inside the section with class="calendarTop"
+// Set innerHTML on monthAndYear to the current date now. This will appear at the top of the calendar inside the section with class="calendarTop".
+// The toLocaleDateString() method returns a string with a language sensitive representation of the date portion of this date
 monthAndYear.innerHTML = now.toLocaleDateString(`en-US`, {
   weekday: 'long',
   year: 'numeric',
@@ -18,6 +18,34 @@ monthAndYear.innerHTML = now.toLocaleDateString(`en-US`, {
   day: 'numeric',
 });
 
+// The function below parses the number of days in the specified month. It will be called dependent on the month selected from the stored now variable. The parameters `year`, and `month`, will be substituted with `MonthYear` and `monthNum` and as arguments
+
+function numDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+  }
+
+// GET the CURRENT YEAR
+let currentYear = now.getFullYear();
+
+// GET the CURRENT MONTH NUMBER
+// Add 1 because January is month number 0
+// We need the .getMonth() + 1 method so that the month and starting day will be correct
+let currentMonthNum = now.getMonth()+1;
+console.log(`The current month number is ${currentMonthNum}, the current day number is ${now.getDate()}`)
+
+// The btnThirtyOn variable will control whether a month having 31 days instead of 30 will display. This will be called in the conditional below
+const btnThirtyOne = document.querySelector("#dayThirtyOne");
+
+// Create a conditional to detect the number of days in the current month, then append the extra day (day 31) to the calendar, the default being 30 days. The variable is in global scope at the top of the script.
+
+if (numDaysInMonth(currentYear, currentMonthNum) === 31) {
+    btnThirtyOne.innerHTML =`<time datetime="${now}">31</time>`;
+    btnThirtyOne.style.display = `flex`;
+  } else {
+    btnThirtyOne.style.display = `none`;
+};
+
+// Set up the CSS grid to switch over to the column respresented dependent on the First day of the month; ie Sunday = 1, Monday = 2, etc. Need to set the start day of the month and add the grid-column class to the index of the month's start day, plus 1
 
 let firstDayName = new Date(now.getFullYear(), now.getMonth(), 1);
 
@@ -25,9 +53,24 @@ let monthStart = document.querySelector(`#dayOne`);
 
 monthStart.style = `grid-column: ${firstDayName.getDay()+1};`;
 
+// Get the current day and add a background style to its box to highlight it
+let dayNumber = document.querySelectorAll(`button > span`);
 
-// Make the Button 31 variable to control whether a month having 31 days instead of 30 will display. This will be called in the conditional embedded in the showDate() callback
-const btnThirtyOne = document.querySelector("#dayThirtyOne");
+dayNumber.forEach((el) => {
+
+  if(el.textContent == now.getDate()) {
+
+    if(!el.parentElement.classList.contains(`bgColorOn`)) {
+       el.parentElement.classList.add(`bgColorOn`);
+    }
+  } else {
+    el.parentElement.classList.remove(`bgColorOn`);
+  }
+});
+
+/* ******* START SUBMIT BUTTON ********** */
+
+// Get the value of the date input field, assign it to a variable. Create an event listener for the form to listen for the submit event, then prevent the default action of the form. This will prevent the page from reloading when the form is submitted.
 
 // Initiate the inputDate Field's submit button
 const submitDate = document.querySelector(`#submitDate`)
@@ -35,124 +78,53 @@ const submitDate = document.querySelector(`#submitDate`)
 // Add an event listener with click event and callback parameters
 submitDate.addEventListener(`click`, showDate);
 
-// NOTE That function declarations are hoisted so I can activate the callback function above the month names array
-
+// Create a function to show the date
 function showDate() {
-
   // Get the input element by its id and store its value
   let dateInputvalue = document.querySelector(`#dateInputField`).value;
-  console.log(dateInputvalue, typeof(dateInputvalue))
 
-  // Create a new Date object from the stored input value
-  let dateInput = new Date(dateInputvalue);
-  console.log(dateInput, typeof(dateInput))
-
-  // Format the date as "March 12 2024"
-  let formattedDate = `${dateInput.toLocaleDateString('en-US', { month: 'long' })} ${dateInput.getDate()} ${dateInput.getFullYear()}`;
-
-  console.log(`${formattedDate} is the date, and it is a ${typeof(formattedDate)}`);
+  // Reassign the now variable to the dateInputvalue
+  now = new Date(dateInputvalue);
   
-  // Add the formatted date to the monthAndYear element, which is the element with id="month"
-  monthAndYear.innerHTML = `${formattedDate}`
+  monthAndYear.innerHTML = now.toLocaleDateString(`en-US`, {  
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
-  // GET the CURRENT MONTH NUMBER
-  // Add 1 because January is month number 0
-  // We need the .getMonth() + 1 method so that the month and starting day will be correct
-  let currentMonthNum = dateInput.getMonth()+1;
-  console.log(`Current month num is ${currentMonthNum}, date input plus 1 is ${dateInput.getDate()}`)
+  // we can use the new Date(dateInputvalue) to get the chosen month and year and use the numDaysInMonth function as a callback to get the number of days in that month
 
-  // Get the current month relative to the array, calling its index number and value. Using the dateInput varialble here
-  let currentMonth = months[dateInput.getMonth()];
-  console.log(currentMonth)
-
-  let currentYear = dateInput.getFullYear();
-  console.log(currentYear)
-
-  // Get the element with id="numOfDays"
-  let numOfDays = document.querySelector(`#numOfDays`);
-
-  numOfDays.innerHTML = ` - has ${numDaysInMonth(
-    currentYear,
-    currentMonthNum
-  )} Days`;
-
-  // Create a conditional to detect the number of days in the current month, then append the extra day (day 31) to the calendar, the default being 30 days. The variable is in global scope at the top of the script.
-
-  if (numDaysInMonth(currentYear, currentMonthNum) === 31) {
-    btnThirtyOne.innerHTML =`<time datetime="${dateInput}">31</time>`;
+  if (numDaysInMonth(now.getFullYear(), now.getMonth()+1) === 31) {
+    btnThirtyOne.innerHTML =`<time datetime="${now}">31</time>`;
     btnThirtyOne.style.display = `flex`;
   } else {
-    // btnThirtyOne.innerHTML =`<time datetime="${dateInput}"></time>`;
     btnThirtyOne.style.display = `none`;
-  };
+};
 
-// Set up the CSS grid to switch over to the column respresented dependent on the First day of the month; ie Sunday = 1, Monday = 2, etc. Need to set the start day of the month and add the grid-column class to the index of the month's start day, plus 1
+// Reassign the firstDayName variable to the new dateInputvalue
+firstDayName = new Date(now.getFullYear(), now.getMonth(), 1);
 
-  const firstDayName = new Date(dateInput.getFullYear(), dateInput.getMonth(), 1);
+// Reassign the monthStart variable to the new dateInputvalue
 
-  let monthStart = document.querySelector(`#dayOne`);
+monthStart = document.querySelector(`#dayOne`);
 
-  monthStart.style = `grid-column: ${firstDayName.getDay()+1};`;
+monthStart.style = `grid-column: ${firstDayName.getDay()+1};`;
 
-  console.log(
-    `${currentMonth} 1, ${currentYear} is a ${
-      daysOfTheWeek[firstDayName.getDay()]
-    }`
-  );
+// Get the chosen day and add a background style to its box to highlight it
+dayNumber.forEach((el) => {
 
-  const dayNumber = document.querySelectorAll(`button > span`);
+  if(el.textContent == now.getDate()) {
 
-  dayNumber.forEach((el) => {
-    // console.log(el.textContent, typeof(el.textContent));
-
-    if(el.textContent == dateInput.getDate()) {
-      console.log(el.textContent == dateInput.getDate(), `${el.textContent}, ${dateInput.getDate()}, ${el}, ${dayNumber}`);
-
-      if(!el.parentElement.classList.contains(`bgColorOn`)) {
-        el.parentElement.classList.add(`bgColorOn`);
-      }
-    } else {
-      // console.log(false)
-      el.parentElement.classList.remove(`bgColorOn`);
+    if(!el.parentElement.classList.contains(`bgColorOn`)) {
+       el.parentElement.classList.add(`bgColorOn`);
     }
-  });
-}
+  } else {
+    el.parentElement.classList.remove(`bgColorOn`);
+  }
+});
 
-// The array of month names below, are strings because it's more efficient to work this way, picking up the month names as array values instead of relying on JS to stringify month names from the Date() object.This applied to the days of the week array as well.
+  return
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+} // End of showDate function
 
-// The days of the week as an array of strings
-const daysOfTheWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-// The function below parses the number of days in the specified month. It will be called dependent on the month selected from the stored dateInput value, inside the "showDate" callback above. The parameters `year`, and `month`, will be substituted with `MonthYear` and `monthNum` and as arguments
-
-function numDaysInMonth(year, month) {
-  return new Date(year, month, 0).getDate();
-}
-
-// For an explanation about Date() parameters
-// SEE: https://www.tutorialsteacher.com/javascript/javascript-date
-
-// ---------- Get the current day and add a background style to its box to highlight it
